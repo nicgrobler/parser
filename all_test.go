15644d33ec35fe0eff0acc080f819c570189feeb
Nic Grobler <nic.grobler2011@gmail.com>
2020-04-29 09:26:58 +0200

    removed dependencies on testify

--- all_test.go
+++ all_test.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func getFakeInput() *expectedInput {
@@ -14,27 +12,37 @@ func TestInferADGroupName(t *testing.T) {
 	i := expectedInput{Environment: "boogie", Role: "Admin", ProjectName: "extra-good"}
 	got := inferADGroupName(&i)
 	want := "RES-BOOGIE-OPSH-ADMIN-EXTRA_GOOD"
-	assert.Equal(t, want, got, "should be equal")
+	if want != got {
+		t.Errorf("wanted %s, but got %s: \n", want, got)
+	}
 }
 
 func TestCreateTouchfileName(t *testing.T) {
 	i := expectedInput{Environment: "boogie"}
 	got := createTouchfileName(&i)
 	want := "OPSH_ENV.BOOGIE"
-	assert.Equal(t, want, got, "should be equal")
+	if want != got {
+		t.Errorf("wanted %s, but got %s: \n", want, got)
+	}
 }
 
 func TestLookupRole(t *testing.T) {
 	// should be ok
 	got := lookupRole("Admin")
 	want := "admin"
-	assert.Equal(t, want, got, "should be equal")
+	if want != got {
+		t.Errorf("wanted %s, but got %s: \n", want, got)
+	}
 	// should return nothing
 	var errorMessage string
 	exitLog = func(message string) { errorMessage = message }
 	got = lookupRole("Administrator")
 	want = ""
-	assert.Equal(t, want, got, "should be equal")
+	if want != got {
+		t.Errorf("wanted %s, but got %s: \n", want, got)
+	}
 	errorShouldBe := "invalid user type specified"
-	assert.Equal(t, errorShouldBe, errorMessage, "should be equal")
+	if want != got {
+		t.Errorf("wanted %s, but got %s: \n", errorShouldBe, errorMessage)
+	}
 }
